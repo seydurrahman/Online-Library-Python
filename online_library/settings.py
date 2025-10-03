43d377123c070d6422c724daca26d1930d61fa2f
Seydur <seydur.rahman82@gmail.com>
2025-10-03 19:49:36 +0600

    CSRF settings done

--- online_library/settings.py
+++ online_library/settings.py
@@ -22,6 +22,9 @@ INSTALLED_APPS = [
 ]
 
 ALLOWED_HOSTS = ["online-library-python.onrender.com"]
+CSRF_TRUSTED_ORIGINS = [
+    "https://online-library-python.onrender.com",
+]
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
